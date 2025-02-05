d2a222be2b68afc1f42cb092b09bc01947eaa4d3
DobryDzus <pschcamera@gmail.com>
2025-02-05 14:25:33 +0100

    ds

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>imgNest</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+    <!-- Header -->
+    <header>
+        <a class="logo-nav" href="/"><img src="img/pokehublogotext.svg" alt="logo"></a>
+        <nav class="navv">
+            <ul class="navbar">
+
+                <li><a class="link-nav" href="#akce">Akce</a></li>
+                <li><a class="link-nav" href="grading.html">Asistovaný grading</a></li>
+                <li><a class="link-nav" href="vykup.html">Výkup</a></li>
+                <div class="divider-cart"></div>
+                <a href="/kosik.html"><i id="cart-nav" class="material-symbols-outlined">shopping_cart</i></a>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
