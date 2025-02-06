3b691083cef464750c52dbb18124d63dec2366d0
DobryDzus <pschcamera@gmail.com>
2025-02-06 11:19:53 +0100

    header

--- index.php
+++ index.php
@@ -3,21 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>imgNest</title>
-    <link rel="stylesheet" href="style.css" type="text/css">
+    <title>imgnest</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <!-- Header -->
     <header>
-        <a class="logo-nav" href="/"><img src="img/pokehublogotext.svg" alt="logo"></a>
         <nav class="navv">
             <ul class="navbar">
-
-                <li><a class="link-nav" href="#akce">Akce</a></li>
-                <li><a class="link-nav" href="grading.html">Asistovaný grading</a></li>
-                <li><a class="link-nav" href="vykup.html">Výkup</a></li>
+                <li><a class="logo-nav" href="/"><img src="img/imgnestlogotext.svg" alt="logo"></a></li>
+                <li><a class="link-nav" href="#">Akce</a></li>
+                <li><a class="link-nav" href="#">Asistovaný grading</a></li>
+                <li><a class="link-nav" href="#">Výkup</a></li>
                 <div class="divider-cart"></div>
-                <a href="/kosik.html"><i id="cart-nav" class="material-symbols-outlined">shopping_cart</i></a>
+                <a href="#"><i id="cart-nav" class="material-symbols-outlined">Upload</i></a>
             </ul>
         </nav>
     </header>
