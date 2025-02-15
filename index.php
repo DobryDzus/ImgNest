429fa723dac9a6c865862b02ce88d1074e622892
DobryDzus <pschcamera@gmail.com>
2025-02-15 18:39:13 +0100

    modal login form

--- index.php
+++ index.php
@@ -15,9 +15,42 @@
         <div class="nav-right">
             <a class="link-nav" href="#">Support us</a>
             <a class="link-nav" href="#">Discord</a>
-            <a class="upload-but link-nav" href="/user_manag/login.php">Upload <i class="fa-solid fa-upload"></i></a>
+            <button class="upload-but link-nav" id="openModal">Upload <i class="fa-solid fa-upload"></i></button>
         </div>
     </nav>
-</header>
+    </header>
+
+<!-- Modal -->
+<div id="loginModal" class="modal">
+    <div class="modal-content form-container">
+        <span class="close">&times;</span>
+        <h2>Login</h2>
+        <form action="" method="post">
+            <input type="text" name="username" id="username" class="form-input" placeholder="username" required>
+            <input type="password" name="password" id="password" class="form-input" placeholder="password" required>
+            <input type="submit" value="Login" class="form-button">
+            <a href="register.php" class="create-account">Create your Account <i class="fa-solid fa-arrow-right"></i></a>
+        </form>
+    </div>
+</div>
 </body>
+<script>
+    const openModalButton = document.getElementById('openModal');
+    const closeModalButton = document.getElementsByClassName('close')[0];
+    const modal = document.getElementById('loginModal');
+
+    openModalButton.onclick = function () {
+        modal.style.display = 'block';
+    };
+
+    closeModalButton.onclick = function () {
+        modal.style.display = 'none';
+    };
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    };
+</script>
 </html>
