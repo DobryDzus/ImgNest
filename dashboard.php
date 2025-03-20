c4fd2cd93bc86c774c7a3d570432421a1ecd746f
DobryDzus <pschcamera@gmail.com>
2025-03-20 15:57:41 +0100

    pridan blok pro upload, a zaklad vzhledu pro search

--- dashboard.php
+++ dashboard.php
@@ -5,12 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>upload | imgnest</title>
     <link rel="stylesheet" href="style/style.css">
+    <script src="https://kit.fontawesome.com/9139c1e78a.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="srch-fltr-ordr">
-        <div class="search-bar">
-            search
-        </div>
+        <form class="search-bar">
+            <input type="text" class="search-input" placeholder="Search..." name="search">
+            <button type="submit" class="search-button"><i class="fa fa-search"></i></button>
+        </form>
         <div class="filter-bar">
             filter
         </div>
@@ -18,5 +20,20 @@
             order
         </div>
     </div>
+    <div class="container">
+        <div class="upload-area" onclick="document.getElementById('fileInput').click();">
+            click here for upload
+        </div>
+        <form id="uploadForm" enctype="multipart/form-data" method="post" action="upload.php" style="display: none;" accept="image/*">
+            <input type="file" id="fileInput" name="file" onchange="document.getElementById('uploadForm').submit();">
+        </form>
+        <div id="uploadStatus"></div>
+    </div>
+
+    <script>
+        document.getElementById('uploadForm').addEventListener('submit', function(event) {
+            document.getElementById('uploadStatus').innerHTML = 'uploading...';
+        });
+    </script>
 </body>
 </html>
