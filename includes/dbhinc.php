ac92b3585bacd7dabf2bbffd71afa355835b9900
DobryDzus <pschcamera@gmail.com>
2025-03-25 15:16:28 +0100

    fix usernames

--- includes/dbhinc.php
+++ includes/dbhinc.php
@@ -1,11 +1,11 @@
 <?php
 $server = "localhost";
-$username = "root";
+$user = "root";
 $password = "";
 $db = "db";
 
 
-$conn = mysqli_connect($server, $username, $password, $db);
+$conn = mysqli_connect($server, $user, $password, $db);
 
 if (!$conn) {
     die("nefunguje pripojeni: " . mysqli_connect_error());
